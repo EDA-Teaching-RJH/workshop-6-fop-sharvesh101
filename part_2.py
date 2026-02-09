018fcce3589a7eab2a85d60b79def3036f01abac
Sharvesh <sl101@kent.ac.uk>
2026-02-09 12:00:47 +0000

    part 2 has been completed

--- part_2.py
+++ part_2.py
@@ -1,1 +1,17 @@
+rover_status = {"Battery": 100,"Heater": "Off","Camera": "Standby"}
+print(f"Battey Status: {rover_status["Battery"]}%")
 
+print("Updating Status")
+rover_status.update({"Battery": 85})
+rover_status.update({"Heater":"On"})
+
+rover_status["Speed"] = 5
+
+print("\nPrinting...")
+for data in rover_status:
+    print(f"{data}: {rover_status[data]}")
+
+mission_log = [{"Site": "Crater A", "Radiation": "Low", "Water": False},{"Site": "Dune B", "Radiation": "High", "Water": True}]
+
+for log in mission_log:
+    print(f"Site [{log["Site"]}] has [{log["Radiation"]}] radiation.")
